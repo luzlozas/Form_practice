0e8ad788fcd35b88978908c45fcea861381fdd10
luzlozas <78167402+luzlozas@users.noreply.github.com>
2022-10-17 22:36:28 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,25 @@
+let id = (id) => document.getElementById(id);
+
+let classes = (classes) => document.getElementsByClassName(classes);
+
+let username = id("username"),
+  email = id("email"),
+  password = id("password"),
+  form = id("form"),
+  errorMsg = classes("error");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  engine(username, 0, "User cannot be blank");
+  engine(email, 1, "Email cannot be blank");
+  engine(password, 2, "Password cannot be blank");
+});
+
+let engine = (id, serial, message) => {
+  if (id.value.trim() === "") {
+    errorMsg[serial].innerHTML = message;
+  } else {
+    errorMsg[serial].innerHTML = "";
+  }
+};
